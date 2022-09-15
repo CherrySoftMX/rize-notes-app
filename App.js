7eeeec52c5ac657b5135d2cfe37c6d7ef44c9b90
Eusebio Ajas Santos <eusebioajas6@gmail.com>
2022-09-15 15:01:58 -0500

    init rize-app project

---
+++ App.js
@@ -0,0 +1,27 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  Text,
+  SafeAreaView
+} from 'react-native';
+
+
+/* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
+ * LTI update could not be added via codemod */
+
+const App = () => {
+  return (
+    <SafeAreaView style={{flex: 1, justifyContent: "center", alignItems: "center"}}>
+      <Text>Hello Voctor 🎉</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
