a86deb514aa770c06c87d8214fdfe5c2db062d0e
Eusebio Ajas Santos <eusebioajas6@gmail.com>
2022-10-22 22:43:34 -0500

    added more color codes

--- src/design/tokens/colors.ts
+++ src/design/tokens/colors.ts
@@ -3,6 +3,11 @@ const colors = {
   lightGreen: '#1FC0A9',
   darkGreen: '#0A8877',
   pureWhite: '#ffffff',
+  black: '#000000',
+  greyNickel: '#C4C4C4',
+  mediumGrey: '#7E7D84',
+  shadowGrey: '#0000001a',
+  yellowishMedium: '#FEB95A',
 };
 
 export { colors };
