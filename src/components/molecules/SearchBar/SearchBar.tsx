10eb86407c95253204ba299b876af50e93311dc0
HikingCarrot7 <ricardoibarra2044@gmail.com>
2022-10-23 13:05:32 -0500

    Added shadow to search bar component

--- src/components/molecules/SearchBar/SearchBar.tsx
+++ src/components/molecules/SearchBar/SearchBar.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HStack } from '@react-native-material/core';
+import { Flex, Surface } from '@react-native-material/core';
 import { colors } from '../../../design/tokens';
 import { Alert, Text, TextInput } from 'react-native';
 import { styles } from '@molecules/SearchBar/SearchBar.style';
@@ -9,22 +9,30 @@ import { MenuOption } from 'react-native-popup-menu';
 
 export const SearchBar = () => {
   return (
-    <HStack style={{ margin: 5 }}>
-      <IconButtonPopupMenu
-        style={styles.leadingFilterButton}
-        iconName="filter-sharp"
-        iconColor={colors.pureWhite}>
-        <MenuOption disabled={true}>
-          <Text style={{ color: colors.eerieBlack }}>Filter by:</Text>
-        </MenuOption>
-        <MenuOption onSelect={() => Alert.alert('Link')} text="Link" />
-        <MenuOption onSelect={() => Alert.alert('URL')} text="URL" />
-      </IconButtonPopupMenu>
-      <TextInput placeholder="Search" style={styles.textInput} />
-      <IoniconButton
-        style={styles.trailingSearchButton}
-        iconName="search-sharp"
-      />
-    </HStack>
+    <Surface elevation={4} category="medium" style={{ margin: 10 }}>
+      <Flex direction="row" style={styles.container}>
+        <FilterButton />
+        <TextInput placeholder="Search" style={styles.textInput} />
+        <IoniconButton
+          style={styles.trailingSearchButton}
+          iconName="search-sharp"
+        />
+      </Flex>
+    </Surface>
+  );
+};
+
+const FilterButton = () => {
+  return (
+    <IconButtonPopupMenu
+      style={styles.leadingFilterButton}
+      iconName="filter-sharp"
+      iconColor={colors.pureWhite}>
+      <MenuOption disabled={true}>
+        <Text style={{ color: colors.eerieBlack }}>Filter by:</Text>
+      </MenuOption>
+      <MenuOption onSelect={() => Alert.alert('Link')} text="Link" />
+      <MenuOption onSelect={() => Alert.alert('URL')} text="URL" />
+    </IconButtonPopupMenu>
   );
 };
