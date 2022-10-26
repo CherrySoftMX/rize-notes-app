68a3c042ec637aae9d311333213fb727eca61342
HikingCarrot7 <ricardoibarra2044@gmail.com>
2022-10-26 13:06:50 -0500

    Added Screen title atom

---
+++ src/components/atoms/ScreenTitle/ScreenTitle.style.ts
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native';
+import { colors, fontSize } from '../../../design/tokens';
+
+export const styles = StyleSheet.create({
+  label: {
+    color: colors.darkGunmetal,
+    fontSize: fontSize.xl,
+    fontWeight: '800',
+    alignSelf: 'center',
+    marginVertical: 8,
+  },
+});
