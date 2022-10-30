16489e5aaeeaed9ec82e87cec31b0f93310e83bf
Eusebio Ajas Santos <eusebioajas6@gmail.com>
2022-10-30 13:32:12 -0600

    updated Folder and FolderList styles

--- src/components/organisms/Folder/Folder.tsx
+++ src/components/organisms/Folder/Folder.tsx
@@ -1,39 +1,38 @@
-import { CustomIcon } from '@atoms/CustomIcon/CustomIcon';
-import { Box, Flex, Text } from '@react-native-material/core';
-import { colors } from '../../../design/tokens/colors';
 import React from 'react';
-import { styles } from './Folder.style';
-import { ProgressBar } from '@atoms/ProgressBar/ProgressBar';
+import { Alert, Text } from 'react-native';
+import { Box, Flex, HStack, Spacer, VStack } from '@react-native-material/core';
+import { CustomIcon } from '@atoms/CustomIcon/CustomIcon';
+import { FolderProps } from './props';
 import { IconButtonPopupMenu } from '@molecules/IconButtonContextMenu';
 import { MenuOption } from 'react-native-popup-menu';
-import { Alert } from 'react-native';
+import { ProgressBar } from '@atoms/ProgressBar/ProgressBar';
+import { colors } from '../../../design/tokens/colors';
+import { styles } from './Folder.style';
 
-interface FolderProps {
-  title: string;
-  numberOfNotes: number;
-  folderColor: string;
-}
-
-export const Folder = ({ title, numberOfNotes, folderColor }: FolderProps) => {
+export const Folder = ({
+  title,
+  numberOfNotes,
+  folderColor,
+  isProgressBar,
+}: FolderProps) => {
   return (
-    <Flex fill direction="row" wrap justify="center" style={styles.card}>
-      <Box>
-        <CustomIcon name="folder" size={75} color={folderColor} />
-        <Box ml={10}>
+    <Flex fill style={styles.container} p={6}>
+      <HStack>
+        <CustomIcon name="folder" size={50} color={folderColor} />
+        <Spacer />
+        <IconButtonPopupMenu iconName="menu" iconColor={colors.greyNickel}>
+          <MenuOption onSelect={() => Alert.alert('Edit')} text="Edit" />
+          <MenuOption onSelect={() => Alert.alert('Delete')} text="Delete" />
+        </IconButtonPopupMenu>
+      </HStack>
+      <VStack m={8} spacing={8}>
+        <Box>
           <Text style={styles.title}>{title}</Text>
           <Text style={styles.subtitle}>{numberOfNotes}</Text>
         </Box>
-      </Box>
-      <IconButtonPopupMenu
-        iconName="menu"
-        iconColor={colors.greyNickel}
-        style={styles.icon}>
-        <MenuOption onSelect={() => Alert.alert('Edit')} text="Edit" />
-        <MenuOption onSelect={() => Alert.alert('Delete')} text="Delete" />
-      </IconButtonPopupMenu>
-      <Box>
-        <ProgressBar completed={30} />
-      </Box>
+        <Spacer />
+        <Box>{isProgressBar && <ProgressBar completed={numberOfNotes} />}</Box>
+      </VStack>
     </Flex>
   );
 };
--- src/components/organisms/FolderList/FolderList.tsx
+++ src/components/organisms/FolderList/FolderList.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import { Folder } from '@organisms/Folder/Folder';
 import { FlatList, View } from 'react-native';
+import { Folder } from '@organisms/Folder/Folder';
 import { styles } from './FolderList.style';
-import FOLDER from '../../../library/services/Folders.json';
+import DATA from '../../../library/services/Folders.json';
 
 export const FolderList = () => {
   return (
     <View style={styles.container}>
       <FlatList
-        data={FOLDER.notes}
+        data={DATA.folders}
         numColumns={2}
         renderItem={({ item }) => <Folder {...item} />}
         keyExtractor={item => item.id}
