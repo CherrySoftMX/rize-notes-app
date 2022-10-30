3000aa8d53c9a3aa76cd451d35a15f4efc50252c
Eusebio Ajas Santos <eusebioajas6@gmail.com>
2022-10-30 13:28:34 -0600

    updated styles

--- src/components/atoms/ProgressBar/ProgressBar.tsx
+++ src/components/atoms/ProgressBar/ProgressBar.tsx
@@ -1,29 +1,33 @@
 import React from 'react';
-import { Box, Flex } from '@react-native-material/core';
+import { Box, VStack } from '@react-native-material/core';
 import { Text } from 'react-native';
 import { colors } from '../../../design/tokens/colors';
 import { styles } from './ProgressBar.style';
 
 interface ProgressBarProps {
   completed: number;
+  size?: number;
 }
 
 const TOTAL_NOTES = 50;
 
-export const ProgressBar = ({ completed }: ProgressBarProps) => {
+export const ProgressBar = ({ completed, size }: ProgressBarProps) => {
   return (
-    <Flex fill m={10} items="center" w={120}>
+    <VStack items="center">
       <Box h={10} w="100%" bg={colors.black} style={styles.br}>
         <Box
           h={10}
-          w={`${completed}%`}
+          w={`${(completed / TOTAL_NOTES) * 100}%`}
           bg={colors.darkGreen}
           style={styles.br}
         />
       </Box>
-      <Box>
-        <Text>{`${completed}/${TOTAL_NOTES}`}</Text>
+      <Box m={2}>
+        <Text
+          style={{
+            fontSize: size,
+          }}>{`${completed}/${TOTAL_NOTES}`}</Text>
       </Box>
-    </Flex>
+    </VStack>
   );
 };
