961acae1fca9cc4e645bf2ab893c113c577abdbc
Eusebio Ajas Santos <eusebioajas6@gmail.com>
2022-10-30 13:37:12 -0600

    added FolderScreen and FolderDetails

---
+++ src/screens/FolderScreen/FolderScreen.style.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native';
+import { colors, fontSize } from '../../design/tokens';
+
+export const styles = StyleSheet.create({
+  sectionTitle: {
+    fontSize: fontSize.xl,
+    fontWeight: 'bold',
+    color: colors.darkGunmetal,
+    paddingLeft: 4,
+    margin: 8,
+  },
+});
