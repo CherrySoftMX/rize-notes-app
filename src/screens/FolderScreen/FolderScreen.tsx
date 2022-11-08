270970c12351fb28cde365299744888c198a5b3f
Shadic78 <carlosjosue7889@gmail.com>
2022-11-08 09:52:37 -0600

    Modified folder screen to retrieve the current selected folder

--- src/screens/FolderScreen/FolderScreen.tsx
+++ src/screens/FolderScreen/FolderScreen.tsx
@@ -1,22 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { colors } from '../../design/tokens';
 import { FolderDetails } from '@organisms/FolderDetails';
 import { NoteList } from '@organisms/NoteList/NoteList';
 import { VStack } from '@react-native-material/core';
 import { styles } from './FolderScreen.style';
 import { Text } from 'react-native';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { useNavigation, useRoute, RouteProp } from '@react-navigation/native';
+import { RootStackParamList } from '@screens/RootStackParams';
+import { FolderInterface } from '../../library/interfaces/FolderInterface';
+import { getFolderById } from '../../library/services/FoldersService';
+import { NoteInterface } from 'library/interfaces/NoteInterface';
+
+type FolderScreenProp = NativeStackNavigationProp<RootStackParamList, 'Folder'>;
+type FolderRouteProp = RouteProp<RootStackParamList, 'Folder'>;
 
 export const FolderScreen = () => {
+  const navigation = useNavigation<FolderScreenProp>();
+  const route = useRoute<FolderRouteProp>();
+  const [folder, setFolder] = useState({} as FolderInterface);
+
+  useEffect(() => {
+    const fetchFolder = async () => {
+      const receivedFolder = await getFolderById(route.params.folderId, true);
+      setFolder(receivedFolder);
+    };
+    fetchFolder();
+  }, [route.params.folderId]);
+
   return (
     <VStack fill>
-      <FolderDetails
-        title="Noticias"
-        numberOfNotes={25}
-        isProgressBar={true}
-        folderColor={colors.yellowishMedium}
-      />
+      <FolderDetails {...folder} />
       <Text style={styles.sectionTitle}>Notes</Text>
-      <NoteList />
+      <NoteList
+        notes={folder.notes as unknown as Array<NoteInterface>}
+        handleClick={() => {}}
+      />
     </VStack>
   );
 };
