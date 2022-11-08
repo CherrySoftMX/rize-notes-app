d53765b11ee932edaf8f6bc47f7a9602a1bcd01a
Shadic78 <carlosjosue7889@gmail.com>
2022-11-08 09:53:09 -0600

    Modified components to receive props

--- src/components/organisms/Note/Note.tsx
+++ src/components/organisms/Note/Note.tsx
@@ -3,19 +3,27 @@ import { CustomIcon } from '@atoms/CustomIcon/CustomIcon';
 import { Flex } from '@react-native-material/core';
 import { HairLine } from '@atoms/HairLine';
 import { NotePreview } from '@organisms/NotePreview/NotePreview';
-import { NoteProps } from './props';
 import { colors } from '../../../design/tokens';
 import { styles } from './Note.style';
 import { IconButtonPopupMenu } from '@molecules/IconButtonContextMenu';
 import { MenuOption } from 'react-native-popup-menu';
 import { Alert } from 'react-native';
+import { NoteInterface } from '../../../library/interfaces/NoteInterface';
 
-export const Note = ({ type, title, content, favorite }: NoteProps) => {
+export const Note = ({ name, content, isLink, isFavorite }: NoteInterface) => {
   return (
     <Flex fill direction="row" self="center" style={styles.card}>
-      {favorite && <HairLine w={4} h="100%" color={colors.yellowishMedium} />}
-      <CustomIcon name={type} size={20} color={colors.pureWhite} isRounded />
-      <NotePreview title={title} content={content} />
+      {isFavorite && <HairLine w={4} h="100%" color={colors.yellowishMedium} />}
+      <CustomIcon
+        name={isLink ? 'link' : 'text-fields'}
+        size={20}
+        color={colors.pureWhite}
+        isRounded
+      />
+      <NotePreview
+        title={name ? name : 'Unnamed'}
+        content={content ? content : 'Empty note'}
+      />
       <IconButtonPopupMenu
         iconName="menu"
         iconColor={colors.greyNickel}
