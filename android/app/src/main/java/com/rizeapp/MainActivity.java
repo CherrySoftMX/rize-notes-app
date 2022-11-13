da051efbfeba612baf767bf2de609d605b3b0620
Shadic78 <carlosjosue7889@gmail.com>
2022-11-12 18:20:57 -0600

    Configured splash screen to remove the white flash when the app opens

--- android/app/src/main/java/com/rizeapp/MainActivity.java
+++ android/app/src/main/java/com/rizeapp/MainActivity.java
@@ -30,7 +30,7 @@ public class MainActivity extends ReactActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
-    SplashScreen.show(this);
+    SplashScreen.show(this, R.style.SplashScreenTheme, true);
     super.onCreate(null);
   }
 
