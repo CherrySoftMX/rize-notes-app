2db07cd981bd710fd98139ca47437f6ba42b7ab7
Shadic78 <carlosjosue7889@gmail.com>
2022-11-15 17:16:09 -0600

    Modified useArrayNavigator to support initial item

--- src/library/hooks/useArrayNavigator/useArrayNavigator.ts
+++ src/library/hooks/useArrayNavigator/useArrayNavigator.ts
@@ -1,8 +1,24 @@
 import { useEffect, useState } from 'react';
 
-export const useArrayNavigator = <T>(items: T[], initialIndex = 0) => {
+export const useArrayNavigator = <T>(
+  items: T[],
+  initialIndex = 0,
+  initialItem: T,
+) => {
   const [currentIndex, setCurrentIndex] = useState(initialIndex);
-  const [currentItem, setCurrentItem] = useState(items[initialIndex]);
+  const [currentItem, setCurrentItem] = useState(
+    initialItem ? initialItem : items[initialIndex],
+  );
+
+  useEffect(() => {
+    if (initialItem) {
+      items.map((_, index) => {
+        if (items[index] === initialItem) {
+          setCurrentIndex(index);
+        }
+      });
+    }
+  }, [initialItem]);
 
   useEffect(() => {
     if (items.length) {
