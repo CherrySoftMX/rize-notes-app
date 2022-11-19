6727f7c3a393155ad8f8ebe1866594baa9fc34ed
Shadic78 <carlosjosue7889@gmail.com>
2022-11-19 12:27:40 -0600

    Added an icon to show when a folder is empty

--- src/components/organisms/NoteList/NoteList.style.ts
+++ src/components/organisms/NoteList/NoteList.style.ts
@@ -1,8 +1,14 @@
 import { StyleSheet } from 'react-native';
+import { fontType, fontSize, spacing } from '../../../design/tokens';
 
 export const styles = StyleSheet.create({
   container: {
     flexGrow: 1,
     paddingHorizontal: 20,
+  },
+  emptyText: {
+    fontFamily: fontType.bold,
+    fontSize: fontSize.md,
+    marginTop: spacing.md,
   },
 });
